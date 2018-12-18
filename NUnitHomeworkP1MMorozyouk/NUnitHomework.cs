442cda5e8c5400870bb6eddefd65616481741ce3
Maxim Morozyouk <gonnatestyourstuff@gmail.com>
2018-12-19 00:11:02 +0200

    some commit

---
+++ NUnitHomeworkP1MMorozyouk/NUnitHomework.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace NUnitHomeworkP1MMorozyouk
+{
+    [TestFixture]
+    public class NUnitHomework
+    {
+        IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait=TimeSpan.FromSeconds(10);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            driver.Quit();
+        }
+
+        [SetUp]
+        public void Login()
+        {
+            driver.Url = "https://atata-framework.github.io/atata-sample-app/#!/signin";
+            driver.FindElement(By.Id("email")).SendKeys("admin@mail.com");
+            driver.FindElement(By.Id("password")).SendKeys("abc123");
+            driver.FindElement(By.CssSelector("input[value='Sign In']")).Click();
+        }
+
+        [TearDown]
+        public void Logout()
+        {
+            driver.FindElement(By.ClassName("navbar-right")).Click();
+            driver.FindElement(By.LinkText("Sign Out")).Click();
+        }
+
+        [Test]
+        public void VerifyPlansHeader()
+        {
+            driver.FindElement(By.LinkText("Plans")).Click();
+            var pageHeader = driver.FindElement(By.TagName("h1")); 
+
+            Assert.AreEqual("Plans", pageHeader.Text);
+        }
+
+        [Test]
+        public void VerifuThatCalculationsAreCurrect()
+        {
+            driver.FindElement(By.LinkText("Calculations")).Click();
+            driver.FindElement(By.Id("addition-value-1")).SendKeys("2");
+            driver.FindElement(By.Id("addition-value-2")).SendKeys("2");
+            var additionResult = driver.FindElement(By.Id("addition-result"));
+
+            Assert.AreEqual("4", additionResult.GetAttribute("value"));
+        }
+
+        [Test]
+        public void VerifyPricesContainDollarSign()
+        {
+            driver.FindElement(By.LinkText("Products")).Click();
+            var prices = driver.FindElements(By.XPath("//tr/td[2]"));
+
+            foreach(var price in prices)
+            {
+                Assert.That(price.Text, Does.Contain("$"));
+            }
+        }
+    }
+}
---
+++ NUnitHomeworkP1MMorozyouk/SetupClass.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace NUnitHomeworkP1MMorozyouk
+{
+    [SetUpFixture]
+    public class SetupClass
+    {
+        string testDirPath;
+
+        [OneTimeSetUp]
+        public void RunBeforeAnyTests()
+        {
+            var testMethodName = TestContext.CurrentContext.Test.ClassName;
+            var dateTime = DateTime.Now.ToString("yyyy-MM-dd-HH-mm");
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, $"TEST {dateTime}");
+            Directory.CreateDirectory(path);
+            testDirPath = path;
+        }
+
+        [OneTimeTearDown]
+        public void RunAfterAnyTests()
+        {
+            var sourcePath = testDirPath;
+            var destinationPath = Path.Combine("C:\\TestResults", Path.GetFileName(testDirPath));
+
+            Directory.CreateDirectory(destinationPath);
+        }
+    }
+}
